07ea6dc85542473672657343181b80d42b80f6e9
lundgren84 <lundgren84@hotmail.se>
2016-11-03 10:21:04 +0100

    asd

--- HemmaKväll/Pages/AddMember.aspx.cs
+++ HemmaKväll/Pages/AddMember.aspx.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using EntityFramework_Project;
+using EntityFramework_Project.CRUDS;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -11,7 +13,29 @@ namespace HemmaKväll.Pages
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            if (!IsPostBack)
+            {
+                ClearTextBoxes();
+            }
+        }
 
+        private void ClearTextBoxes()
+        {
+            TextBox1.Text = "";
+            TextBox2.Text = "";
+            TextBox3.Text = "";
+            TextBox4.Text = "";
+
+        }
+
+        protected void Button1_Click(object sender, EventArgs e)
+        {
+            tbl_Member member = new tbl_Member();
+            member.FirstName = TextBox1.Text;
+            member.LastName = TextBox2.Text;
+            member.PhoneNumber = TextBox3.Text;
+            member.CSN = TextBox4.Text;
+            CRUD_AddMember.NewMember(member);
         }
     }
 }
