bd8bcd117c956c5e0ceb7d61bb7d56deada50302
lundgren84 <lundgren84@hotmail.se>
2016-11-03 11:13:09 +0100

    asd

--- HemmaKväll/Pages/SearchMember.aspx
+++ HemmaKväll/Pages/SearchMember.aspx
@@ -3,6 +3,19 @@
 <asp:Content ContentPlaceHolderID="ContentPlaceHolder1" runat="server">
     <h1>Search member</h1>
     <div class="container">
+        <div class="row">
+            <div class="col-xs-2">
+                <asp:DropDownList ID="DropDownList1" runat="server"></asp:DropDownList>
+            </div>
+            <div class="col-xs-2">
+                <asp:TextBox ID="TextBox1" runat="server"></asp:TextBox>
+            </div>
+            <div class="col-xs-2">
+                <asp:Button ID="Button1" runat="server" Text="Search" />
+            </div>
+        </div>
+
+        <h2>All Members</h2>
         <div runat="server" id="AllMembers">
             <div class="row">
                 <div class="col-xs-1"></div>
--- HemmaKväll/Pages/SearchMember.aspx.cs
+++ HemmaKväll/Pages/SearchMember.aspx.cs
@@ -15,21 +15,32 @@ namespace HemmaKväll.Pages
         protected void Page_Load(object sender, EventArgs e)
         {
             fillAllMembers();
+            fillDroppdown();
+        }
+
+        private void fillDroppdown()
+        {
+           //DropDownList1.DataSource
         }
 
         private void fillAllMembers()
         {
             List<tbl_Member> MembersList = CRUD_Add_Search_Member.GetAllMembers();
             StringBuilder sb = new StringBuilder();
-
-
-            sb.Append($@"     <div class='row'>
-                < div class='col-xs-1'></div>
-                <div class='col-xs-1'></div>
-                <div class='col-xs-1'></div>
+            foreach (var item in MembersList)
+            {
+                sb.Append($@"     <div class='row'>
+                <div class='col-xs-2'>ID: {item.Member_ID}</div>
+                <div class='col-xs-2'>{item.LastName}. {item.FirstName}</div>
+                <div class='col-xs-2'>SSN: {item.CSN}</div>
+                <div class='col-xs-2'>Phone: {item.PhoneNumber}</div>
+                <hr/>
             </div>");
+            }
 
-            AllMembers.InnerHtml = "";
+        
+
+            AllMembers.InnerHtml = sb.ToString();
         }
     }
 }
